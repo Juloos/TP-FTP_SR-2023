ad4b25d4de065a9c7f24ed4fdbd338c6e744fc33
tom7438 <tom192010@live.fr>
2023-03-23 13:01:12 +0100

    Ajout structures

---
+++ protocoles.h
@@ -0,0 +1,29 @@
+#ifndef TP_FTP_SR_2023_PROTOCOLES_H
+#define TP_FTP_SR_2023_PROTOCOLES_H
+
+#define MAXLEN 4096
+
+typedef enum {
+    OP_LS,
+    OP_CD,
+    OP_PWD,
+    OP_GET,
+    OP_PUT,
+    OP_BYE
+} Op;
+
+typedef enum {
+    ERREUR_OK,
+    ERREUR_KO
+} Erreur;
+
+typedef struct {
+    Op code;
+    char arg[MAXLEN];
+} Requete;
+
+typedef struct {
+    Erreur code; // 0 = OK, 1 = KO
+} Reponse;
+
+#endif //TP_FTP_SR_2023_PROTOCOLES_H
