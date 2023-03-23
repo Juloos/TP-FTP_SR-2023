5a016fc3004261bdcd874483d2acedaf895d42e2
tom7438 <tom192010@live.fr>
2023-03-23 13:22:49 +0100

    Ajout création de la structure requete

--- CLIENT.c
+++ CLIENT.c
@@ -1,4 +1,6 @@
-#include "csapp.h"
+#include "Headers/csapp.h"
+#include "protocoles.h"
+#include <string.h>
 
 #define PORT 4242
 
@@ -20,6 +22,32 @@ int main(int argc, char **argv) {
     Rio_readinitb(&rio, clientfd);
 
     while (Fgets(buf, MAXLINE, stdin) != NULL) {
+
+        Requete req = {OP_BYE, ""};
+        if (strncmp(buf, "get ", 4) == 0) {
+            req.code = OP_GET;
+        } else if (strncmp(buf, "bye ", 4) == 0) {
+            req.code = OP_BYE;
+        } else {
+            printf("Commande non implémentée\n");
+            continue;
+        }
+
+        switch(req.code) {
+            case OP_GET:
+                if(strlen(buf) < 5) {
+                    fprintf(stderr, "Il manque un argument\n");
+                    break;
+                }
+                strcpy(req.arg, buf + 4);
+                break;
+            case OP_BYE:
+                strcpy(req.arg, "");
+                break;
+            default:
+                fprintf(stderr, "Commande non implémentée\n");
+        }
+
         Rio_writen(clientfd, buf, strlen(buf));
         if (Rio_readlineb(&rio, buf, MAXLINE) > 0)
             Fputs(buf, stdout);
