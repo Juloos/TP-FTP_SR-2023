cf66d5e7dacda840ff77487b2254b94e15e0e82d
Juloos <juloos.sfr@gmail.com>
2023-03-23 10:44:05 +0100

    Base pour le CLIENT

---
+++ CLIENT.c
@@ -0,0 +1,31 @@
+#include "csapp.h"
+
+#define PORT 4242
+
+int main(int argc, char **argv) {
+    int clientfd;
+    char *host, buf[MAXLINE];
+    rio_t rio;
+
+    if (argc != 2) {
+        fprintf(stderr, "usage: %s <host>\n", argv[0]);
+        exit(EXIT_SUCCESS);
+    }
+    host = argv[1];
+
+    clientfd = Open_clientfd(host, PORT);
+
+    printf("Connected\n");
+
+    Rio_readinitb(&rio, clientfd);
+
+    while (Fgets(buf, MAXLINE, stdin) != NULL) {
+        Rio_writen(clientfd, buf, strlen(buf));
+        if (Rio_readlineb(&rio, buf, MAXLINE) > 0)
+            Fputs(buf, stdout);
+        else
+            break;
+    }
+    Close(clientfd);
+    exit(EXIT_SUCCESS);
+}
